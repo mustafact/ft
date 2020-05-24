984976d99c387187b86ffc527024dbf7899ac55d
mustafact <51968589+mustafact@users.noreply.github.com>
2020-05-24 04:10:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 class Cars {
 
+
+
+
     constructor() {
 
-
-
-        this.allCars = [];
-        this.instanceOfCars = [];
+        this.allCars = this.notes = JSON.parse(localStorage.getItem('cars')) || [];
+        //this.instanceOfCars = [];
         this.instanceOfAddCarBrand = "";
         this.instanceOfCarId = "";
         this.instanceOfAddCarDate = "";
@@ -13,11 +14,31 @@ class Cars {
         this.searchNameUserInput = "";
         this.searchDateUserInput = "";
 
-        this.instantBrandForModal = "",
-            this.instantNameForModal = "",
-            this.instantAmountForModal = "",
-            this.instantDateForModal = "",
-            this.sortOrder = false;
+        this.isDateValid = false;
+        this.isFormValid = false;
+
+        this.instantFilteredData = [];
+
+
+        this.instantBrandForModal = "";
+        this.instantNameForModal = "";
+        this.instantAmountForModal = "";
+        this.instantDateForModal = "";
+        this.sortOrder = false;
+
+
+        // calculation related
+        this.totalSales = "";
+        this.totalSalesToyota = "";
+        this.totalSalesAudi = "";
+
+        this.totalNotPaid = "";
+
+
+
+
+
+
 
 
 
@@ -32,7 +53,6 @@ class Cars {
         this.$inputSearchName = document.querySelector("#input-search-name");
         this.$inputSearchDate = document.querySelector("#input-search-date");
 
-        this.$addButton = document.querySelector("#add-button")
 
         this.$dataTable = document.querySelector("#data-table");
 
@@ -58,13 +78,13 @@ class Cars {
         this.$modalDeleteDate = document.querySelector("#modal-delete-date");
 
 
+        this.$buttonNavSee = document.querySelector("#button-nav-see");
 
 
+        this.$formNavSearch = document.querySelector("#form-nav-search");
 
-
-
-
-
+        this.calculations();
+        this.saveData();
         this.addEventListeners();
 
     }
@@ -72,25 +92,32 @@ class Cars {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     addEventListeners() {
+
         document.body.addEventListener("click", event => {
 
             console.log(event.target);
             this.adjustInstanceOfAddCarBrand(event);
-            this.addCarsConstructCarsDataArray(event);
-
-
-
+            //this.addCarsConstructCarsDataArray(event);
 
             this.handleSearchInputCombined(event);
 
-
-
-
-
-
-
-
+            this.isInputDataValid(event);
 
             this.selectOneCustomerClickingEdit(event);
 
@@ -102,19 +129,15 @@ class Cars {
 
 
 
-            this.renderData();
 
 
             this.sortData(event);
 
+            //this.filterData();
 
 
 
-
-
-
-
-
+            this.handleSeeButtonClickOnNavbar(event);
 
         });
     }
@@ -124,10 +147,46 @@ class Cars {
 
 
 
+
+
+
+
+
+
+    handleSeeButtonClickOnNavbar(event) {
+        if (event.target.id === "button-nav-see") {
+            event.preventDefault();
+            this.$formNavSearch.classList.toggle("d-none");
+            this.$inputAddCarsForm.classList.add("d-none");
+            console.log("search clicked");
+            if (this.allCars.length > 0) {
+                this.$table.classList.remove("d-none");
+            }
+
+            this.renderData(this.allCars);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     adjustInstanceOfAddCarBrand(event) {
         if (event.target.classList.contains("add-cars")) {
             this.instanceOfAddCarBrand = event.target.textContent; // determine the car brand selector
-            this.$textareaAddCarsButton.textContent = this.instanceOfAddCarBrand; // adjust the brand name on the button
+            //this.$textareaAddCarsButton.textContent = this.instanceOfAddCarBrand; // adjust the brand name on the button
+            this.$textareaAddCars.placeholder = `paste ${this.instanceOfAddCarBrand} customers from excel in the format: \nname | amount\nname | amount\n...\n...`; // adjusting the placeholder dynanically
             this.$inputAddCarsForm.classList.remove("d-none");
             this.$table.classList.add("d-none");
         }
@@ -135,12 +194,28 @@ class Cars {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // check the validity of and transform the customer names and amounts for 
+    // our objects array
+
     transformDataEnteredIntoTextareaIntoArrayOfObjects() {
 
-
-
         if (this.$textareaAddCars.value.includes("\t") &&
-            this.$textareaAddCars.value.includes("\n")) {
+            this.$textareaAddCars.value.includes("\n") &&
+            this.$textareaAddCars.value !== "" &&
+            this.$inputAddCarsDate.value !== "") {
 
             let dataInputIntoAddCarTextArea = this.$textareaAddCars.value;
 
@@ -158,23 +233,32 @@ class Cars {
                     id: this.allCars.length > 0 ? this.allCars[this.allCars.length - 1].id + 1 : 1
                 };
 
+
+
                 this.allCars = [...this.allCars, obj];
+
+
             });
 
-
-
-        } else {
-            console.log("please make sure copy paste from excel in two rows");
-            // here we might use alerts
         }
 
     }
 
 
 
+
+
+
+
+
+
+
+
+
+
+    // check the validity of and format the date to keep in our objects array
+
     transformDateEntered() {
-
-
 
         let lastTwoDigitsOfCurrentYear = new Date().getFullYear().toString().slice(-2);
 
@@ -196,29 +280,115 @@ class Cars {
             this.instanceOfAddCarDate = inputDate;
 
 
-        } else {
-            console.log("please enter date accordingly to the date format");
-            // here we might use alerts
         }
 
     }
 
 
 
-    addCarsConstructCarsDataArray(event) {
-
-        // only called after this condition and then calls the following dunctions
-        if (event.target.id === "textarea-add-cars-button" && this.instanceOfAddCarBrand !== "") {
 
 
-            this.transformDateEntered();
-            this.transformDataEnteredIntoTextareaIntoArrayOfObjects();
 
-            console.log(this.allCars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // validate and Make the array of objects
+
+    isInputDataValid(event) {
+
+        if (event.target.id === "textarea-add-cars-button") {
+
+
+            let dateInput = this.$inputAddCarsDate.value;
+            let lastTwoDigitsOfCurrentYear = new Date().getFullYear().toString().slice(-2);
+
+            if (dateInput !== "" &&
+                dateInput.length === 8 &&
+                dateInput[2] === "-" &&
+                dateInput[5] === "-" &&
+                dateInput.slice(0, 2) <= lastTwoDigitsOfCurrentYear &&
+                dateInput.slice(3, 5) > 0 &&
+                dateInput.slice(3, 5) <= 12 &&
+                dateInput.slice(-2) > 0 &&
+                dateInput.slice(-2) <= 31
+            ) {
+
+                this.isDateValid = true;
+
+
+            } else {
+
+                this.isDateValid = false;
+                alert("date not correct");
+
+            }
+
+            console.log(this.isDateValid);
+
+
+            let carsInput = this.$textareaAddCars.value;
+
+            if (carsInput.includes("\t") &&
+                carsInput.includes("\n") &&
+                carsInput !== "") {
+                this.isFormValid = true;
+            } else {
+                this.isFormValid = false;
+                alert("form not correct");
+
+            }
+
+
+            console.log(this.isFormValid);
+
+            // only if both are true then make the array of objects
+            // otherwise alert the user
+
+            if (this.isDateValid && this.isFormValid) {
+
+                this.transformDateEntered(); // ok
+
+                this.transformDataEnteredIntoTextareaIntoArrayOfObjects();
+
+                console.log(this.allCars);
+
+                this.saveData();
+
+            } else {
+                console.log("INCOMPLETE DATA!!!");
+                return;
+            }
 
         }
 
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -230,10 +400,15 @@ class Cars {
     handleSearchInputCombined(event) {
 
 
-
-
         if (event.target.id === "button-search") {
+
             event.preventDefault();
+
+
+            if (!this.allCars.length > 0) {
+                alert("no customers yet... plase add some customers");
+                return;
+            }
 
             console.log(this.allCars);
 
@@ -248,27 +423,40 @@ class Cars {
             console.log(brandString, nameString, dateString);
 
 
-            let filtered = this.allCars.filter(item =>
-                item.brand.includes(brandString)).filter(item =>
-                item.name.includes(nameString)).filter(item => item.date.includes(dateString));
 
-            // intance of data is different from the all cars data
-            this.instanceOfCars = filtered;
 
-            console.log(filtered);
+            let filtered = this.allCars.filter(veri =>
+                veri.brand.includes(brandString)).filter(veri =>
+                veri.name.includes(nameString)).filter(veri => veri.date.includes(dateString));
 
-            this.renderData();
+            // intance of data is different from the all cars data so...
+
+
+            //Data = [...filtered];
+
+
+            this.renderData(filtered);
+
+
+
+
 
         }
+
     }
 
 
 
 
 
-    renderData() {
 
-        let htmlForTable = this.instanceOfCars.map((item, index) =>
+
+
+
+
+    renderData(array) {
+
+        let htmlForTable = array.map((item, index) =>
 
             `
         <tr class="one-customer-row">
@@ -281,12 +469,53 @@ class Cars {
             <td class="one-customer edit-one-customer" data-toggle="modal" data-target="#editModal" data-id="${item.id}">✍︎</td>
             <td class="one-customer delete-one-customer" data-toggle="modal" data-target="#deleteModal" data-id="${item.id}">❌</td>
         </tr>
-
         `
 
         ).join("");
 
         this.$dataTable.innerHTML = htmlForTable;
+
+    }
+
+
+
+
+
+    filterData() {
+
+        console.log("filter called");
+
+        if (!this.allCars.length > 0) {
+            alert("there are no customers yet");
+            return;
+        }
+
+        console.log(this.allCars);
+
+        this.$inputAddCarsForm.classList.add("d-none");
+        this.$table.classList.remove("d-none");
+
+
+        let brandString = this.$inputSearchBrand.value.toLowerCase();
+        let nameString = this.$inputSearchName.value.toLowerCase();
+        let dateString = this.$inputSearchDate.value.toLowerCase();
+
+        console.log(brandString, nameString, dateString);
+
+
+
+
+        let filtered = this.allCars.filter(veri =>
+            veri.brand.includes(brandString)).filter(veri =>
+            veri.name.includes(nameString)).filter(veri => veri.date.includes(dateString));
+
+        // intance of data is different from the all cars data so...
+
+        console.log(filtered);
+
+        //this.instantFilteredData = [...filtered];
+
+        this.renderData(filtered);
 
 
 
@@ -295,25 +524,27 @@ class Cars {
 
 
 
+
+
+
+
+
+
+
+
+
     sortData(event) {
-
-
 
         if (event.target.classList.contains("data-table-head")) {
 
+
             this.sortOrder = !this.sortOrder;
-
-
 
             let sortBy = event.target.textContent.toLowerCase();
 
             console.log(sortBy);
 
-
-
-
-
-            this.instanceOfCars.sort((a, b) => {
+            this.allCars.sort((a, b) => {
                 if (this.sortOrder === true) {
                     if (a[sortBy] < b[sortBy]) return -1;
                     if (a[sortBy] > b[sortBy]) return 1;
@@ -326,11 +557,34 @@ class Cars {
 
             });
 
+            this.filterData();
 
-            this.renderData();
+            //this.renderData(this.allCars);
+
+
+
         }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -340,7 +594,6 @@ class Cars {
 
             let $parent = event.target.parentNode;
 
-
             this.instanceOfCarId = event.target.dataset.id;
 
             this.instantBrandForModal = $parent.children[1].textContent;
@@ -348,12 +601,10 @@ class Cars {
             this.instantAmountForModal = $parent.children[3].textContent;
             this.instantDateForModal = $parent.children[4].textContent;
 
-
             this.$modalInputBrand.value = this.instantBrandForModal;
             this.$modalInputName.value = this.instantNameForModal;
             this.$modalInputAmount.value = this.instantAmountForModal;
             this.$modalInputDate.value = this.instantDateForModal;
-
 
         }
 
@@ -361,13 +612,17 @@ class Cars {
 
     }
 
+
+
+
+
+
+
     savingEditedCustomerViaModal(event) {
 
         // if save button is clicked....
 
         if (event.target.id === "button-modal-save") {
-
-            // do this
 
             // getting the updated values inside the modal by the customer
             console.log(this.$modalInputBrand.value);
@@ -385,6 +640,8 @@ class Cars {
             console.log(brand, name, amount, date);
 
 
+
+
             this.allCars = this.allCars.map(item => {
                 if (item.id === Number(this.instanceOfCarId)) {
                     return {...item, brand: brand, name: name, amount: amount, date: date }
@@ -393,9 +650,9 @@ class Cars {
                 }
             });
 
-            this.instanceOfCars = this.allCars;
+            this.saveData();
+            this.filterData();
 
-            this.renderData();
 
         }
 
@@ -403,16 +660,26 @@ class Cars {
 
 
 
-    selectOneCustomerClickingDelete(event) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+    selectOneCustomerClickingDelete(event) {
 
         if (event.target.classList.contains("delete-one-customer")) {
 
             console.log("delete clicked");
 
-
             let $parent = event.target.parentNode;
-
 
             this.instanceOfCarId = event.target.dataset.id;
 
@@ -421,21 +688,22 @@ class Cars {
             this.instantAmountForModal = $parent.children[3].textContent;
             this.instantDateForModal = $parent.children[4].textContent;
 
-
             this.$modalDeleteBrand.textContent = this.instantBrandForModal;
             this.$modalDeleteName.textContent = this.instantNameForModal;
             this.$modalDeleteAmount.textContent = this.instantAmountForModal;
             this.$modalDeleteDate.textContent = this.instantDateForModal;
 
-
         }
 
-
-
-
-
-
     }
+
+
+
+
+
+
+
+
 
 
     deletingSelectedCustomerViaModal(event) {
@@ -445,16 +713,49 @@ class Cars {
         if (event.target.id === "button-modal-delete-confirm") {
 
             console.log(this.instanceOfCarId);
+            console.log(this.allCars);
 
             this.allCars = this.allCars.filter(item => item.id !== Number(this.instanceOfCarId));
 
-            this.instanceOfCars = this.allCars;
 
-            this.renderData();
+            this.saveData();
+            this.filterData();
 
         }
 
     }
+
+
+
+    saveData() {
+        localStorage.setItem('cars', JSON.stringify(this.allCars));
+    }
+
+
+
+
+
+
+    // CALCULATIONS
+
+
+    calculations() {
+
+        let totalSales = this.allCars.reduce((acc, curr) => {
+            return acc + Number(curr.amount)
+        }, 0)
+
+
+        let totalSalesToyota = this.allCars.filter(item => item.brand === "toyota").reduce((acc, curr) => {
+            return acc + Number(curr.amount)
+        }, 0)
+
+        console.log(totalSales, totalSalesToyota);
+
+
+    }
+
+
 
 
 
